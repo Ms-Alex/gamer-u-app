bfba1b3e6e5c3578336582256fe494181df8a1b9
Ms-Alex <ms.hernandeza1@gmail.com>
2018-10-25 23:37:35 -0400

    clean up files a bit

--- index.js
+++ index.js
@@ -65,33 +65,4 @@ io.on('connection', function (socket) {
         io.emit('RECEIVE_MESSAGE', data);
     })
 
-
-    // client.on('subscribeToTimer', (interval) => {
-    //     console.log('client is subscribing to timer with interval ', interval);
-    //     setInterval(() => {
-    //         client.emit('timer', new Date());
-    //     }, interval);
-    // });
-
-    // client.on('register', handleRegister)
-
-    // client.on('join', handleJoin)
-
-    // client.on('leave', handleLeave)
-
-    // client.on('message', handleMessage)
-
-    // client.on('chatrooms', handleGetChatrooms)
-
-    // client.on('availableUsers', handleGetAvailableUsers)
-
-    // client.on('disconnect', function () {
-    //     console.log('client disconnect...', client.id)
-    //     handleDisconnect()
-    // })
-
-    // client.on('error', function (err) {
-    //     console.log('received error from client:', client.id)
-    //     console.log(err)
-    // })
 })
